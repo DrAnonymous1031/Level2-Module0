cc0b7017e02d9cc7cd5f633ba5058c3934477487
league <league@192.168.1.82>
2026-02-21 16:26:16 -0800

    hehehaha

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -10,9 +10,9 @@ public class _00_ArrayCheatSheet {
 		//2. print the third element in the array
 		System.out.println(kitties[2]);
 		//3. set the third element to a different value
-		
+		kitties[3]=("Gnarpy");
 		//4. print the third element again
-		
+		System.out.println(kitties[3]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		
@@ -21,17 +21,37 @@ public class _00_ArrayCheatSheet {
 		int[] eps = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
 		for(int i=0; i<eps.length; i++) {
-			eps[i]=ran.nextInt(200)+1;
+			eps[i]=ran.nextInt(1000)+1;
 		}
 		//8. without printing the entire array, print only the smallest number in the array
 		int z = 100;
 		for(int i=0;i<eps.length; i++) {
-			System.out.println(eps[i]);
+//			System.out.println(eps[i]);
 			if(eps[i]<z) {
 				z=eps[i];
 			}
 		}
 		System.out.println(z);
+		System.out.println(" ");
+		boolean bool = true;
+		while (bool) {
+			int l = 0;
+			for(int i=0; i<eps.length-1; i++) {
+				if(eps[i]>eps[i+1]) {
+					int v = eps[i];
+					eps[i] = eps[i+1];
+					eps[i+1]=v;
+					l++;
+				}
+			}
+			if(l==0) {
+				bool=false;
+			}
+		}
+		System.out.println(" ");
+		for(int i=0;i<eps.length; i++) {
+			System.out.println(eps[i]);
+		}
 		//9 print the entire array to see if step 8 was correct
 		
 		//10. print the largest number in the array.
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,10 +1,16 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+	public static void main(String[] args) {
+	Robot[]roberts=new Robot[5];
 	//2. create an array of 5 robots.
-
+	for(int i = 0;i<roberts.length;i++) {
+		roberts[i]=new Robot();
+		roberts[i].setY(20);
+	}
 	//3. use a for loop to initialize the robots.
 
 		//4. make each robot start at the bottom of the screen, side by side, facing up
@@ -19,4 +25,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+	}
 }
